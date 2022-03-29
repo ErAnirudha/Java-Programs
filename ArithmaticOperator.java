b4b24928b93c53476b878fe87ff51ee5580dfa25
ErAnirudha <93269088+ErAnirudha@users.noreply.github.com>
2022-03-29 18:53:22 +0530

    Update ArithmaticOperator.java

--- ArithmaticOperator.java
+++ ArithmaticOperator.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
-public class ArithmaticOperator
+import java.util.Scanner;                              //header file
+public class ArithmaticOperator                       // class name    
 {
- public static void main(String[]args){
+ public static void main(String[]args){              //main function start
   float add,sub,mul,div,modulus;
-  Scanner sc=new Scanner(System.in);
+  Scanner sc=new Scanner(System.in);                 //Scanning Syntax     **Scanner sc=new Scanner(System.in);**
   System.out.print("Enter value for X= ");
   float x=sc.nextFloat();
   System.out.print("Enter value for y= ");
